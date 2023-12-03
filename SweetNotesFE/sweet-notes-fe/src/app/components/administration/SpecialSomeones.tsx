868a193ca7461435687c0519a73c1026e95ff7aa
Esau Silva <esausilva85@gmail.com>
2023-12-03 16:23:58 -0600

    FE: Special Someone Admin section layout and CSS

--- SweetNotesFE/sweet-notes-fe/src/app/components/administration/SpecialSomeones.tsx
+++ SweetNotesFE/sweet-notes-fe/src/app/components/administration/SpecialSomeones.tsx
@@ -1,5 +1,41 @@
 import styles from './SpecialSomeones.module.scss';
 
 export function SpecialSomeones(): JSX.Element {
-  return <section className={styles.specialSomeones}>Special Someones</section>;
+  return (
+    <section className={styles.specialSomeones}>
+      <h2>Special Someones</h2>
+      <div className={styles.specialSomeoneDropdownWrapper}>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+        <p className={styles.annotation}>
+          <a href="#">Add Special Someone</a>
+        </p>
+      </div>
+      <form className={styles.noteForm} action="">
+        <label>Note</label>
+        <textarea name="note"></textarea>
+        <p className={styles.annotation}>10 / 50</p>
+        <button className="button--primary" type="submit">
+          Send Note
+        </button>
+      </form>
+      <p>
+        Share this unique link with your <em>Special Someone</em> to access your
+        notes to them:
+      </p>
+      <p>
+        <a href="#">
+          https://domain.com/ss/jmvCFalPuA~nlGp2xnVD_smUdnwV5U)fUmoa51uTJtFQ_
+        </a>
+      </p>
+      <p className={styles.annotation}>
+        Note: Anyone with the <em>Special Someone</em> link will be able to see
+        the notes!
+      </p>
+    </section>
+  );
 }
