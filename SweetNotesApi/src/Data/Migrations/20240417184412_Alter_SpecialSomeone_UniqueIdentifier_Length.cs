0a46ac4515fe0918f0c83a5d58781da744520faf
Esau Silva <esausilva85@gmail.com>
2024-04-17 14:45:05 -0500

    BE: Check in EF migrations

---
+++ SweetNotesApi/src/Data/Migrations/20240417184412_Alter_SpecialSomeone_UniqueIdentifier_Length.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_SpecialSomeone_UniqueIdentifier_Length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UniqueIdentifier",
+                table: "SpecialSomeone",
+                type: "character varying(26)",
+                maxLength: 26,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(45)",
+                oldMaxLength: 45);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UniqueIdentifier",
+                table: "SpecialSomeone",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(26)",
+                oldMaxLength: 26);
+        }
+    }
+}
