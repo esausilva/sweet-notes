44caaee3201cf2e4abf59df6bf2c13bca137c165
Esau Silva <esausilva85@gmail.com>
2024-10-01 22:25:26 -0500

    FE: Refactor useRenderGraphQLErrorList to handle undefined errors

--- SweetNotesFE/sweet-notes-fe/src/app/hooks/useRenderGraphQLErrorList.tsx
+++ SweetNotesFE/sweet-notes-fe/src/app/hooks/useRenderGraphQLErrorList.tsx
@@ -3,7 +3,17 @@ import { GraphQLError } from '@/types';
 export function useRenderGraphQLErrorList({
   errors,
 }: GraphQLError): JSX.Element {
-  if (errors.length > 0) {
+  if (errors === undefined) {
+    return (
+      <div className="error-list">
+        Something went wrong!
+        <br />
+        This should have returned an errors list.
+      </div>
+    );
+  }
+
+  if (errors?.length > 0) {
     return (
       <div className="error-list">
         <ul>
