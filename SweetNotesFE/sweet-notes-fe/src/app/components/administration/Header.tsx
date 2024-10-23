29bc5a75ff0cc171b0ef88c262319d2a22ba6f24
Esau Silva <esausilva85@gmail.com>
2024-10-22 19:16:25 -0500

    FE: Move the navigation out to its own component

--- SweetNotesFE/sweet-notes-fe/src/app/components/administration/Header.tsx
+++ SweetNotesFE/sweet-notes-fe/src/app/components/administration/Header.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import Avatar from 'boring-avatars';
 
 import { Me } from '@/types';
-import { HamburgerIcon } from '@/resources/HamburgerIcon';
-import { Routes } from '@/constants';
+import { Navigation } from '@/component/administration/Navigation';
 
 import styles from './Header.module.scss';
 
 export function Header({ me }: { me: Me }): JSX.Element {
   const [avatarSize, setAvatarSize] = useState<number>(90);
-  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if (typeof window !== `undefined`) {
@@ -18,35 +15,9 @@ export function Header({ me }: { me: Me }): JSX.Element {
     }
   }, []);
 
-  const toggleNavigation = () => setIsNavOpen(!isNavOpen);
-
   return (
     <header className={styles.header}>
-      <HamburgerIcon className={styles.hamburger} onClick={toggleNavigation} />
-      <nav className={`${styles.navigation} ${isNavOpen ? '' : styles.closed}`}>
-        <button
-          aria-label="Close Navigation"
-          name="Close Navigation"
-          onClick={toggleNavigation}
-        >
-          &times;
-        </button>
-        <ul className={styles.navigationItems}>
-          <li>
-            <Link href={`/${Routes.USER_ADMINISTRATION}`}>Administration</Link>
-          </li>
-          <li>
-            <a href="#">Update Password</a>
-          </li>
-          <li>
-            <Link
-              href={`${process.env.NEXT_PUBLIC_API_CLIENT_SIDE_ROOT_URI}/${Routes.USER_LOGOUT}`}
-            >
-              Logout
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
       <h1 className={styles.title}>Sweet Notes</h1>
       <div className={styles.avatar}>
         <p>{me.name}</p>
---
+++ SweetNotesFE/sweet-notes-fe/src/app/components/administration/Navigation.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+import { HamburgerIcon } from '@/resources/HamburgerIcon';
+import { Routes } from '@/constants';
+
+import styles from './Navigation.module.scss';
+
+export function Navigation(): JSX.Element {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+
+  const toggleNavigation = () => setIsNavOpen(!isNavOpen);
+
+  return (
+    <>
+      <HamburgerIcon className={styles.hamburger} onClick={toggleNavigation} />
+      <nav className={`${styles.navigation} ${isNavOpen ? '' : styles.closed}`}>
+        <button
+          aria-label="Close Navigation"
+          name="Close Navigation"
+          onClick={toggleNavigation}
+        >
+          &times;
+        </button>
+        <ul className={styles.navigationItems}>
+          <li>
+            <Link href={`/${Routes.USER_ADMINISTRATION}`}>Administration</Link>
+          </li>
+          <li>
+            <a href="#">Update Password</a>
+          </li>
+          <li>
+            <Link
+              href={`${process.env.NEXT_PUBLIC_API_CLIENT_SIDE_ROOT_URI}/${Routes.USER_LOGOUT}`}
+            >
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
